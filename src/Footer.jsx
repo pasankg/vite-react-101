0f73db1ab322810c171889d7a9de961bc7475bb5
Pasan Gamage <pasankg@gmail.com>
2024-07-28 11:31:53 +1000

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+ return(
+  <footer>
+   <p>Welcome to my site</p>
+   <p> &copy;{new Date().getFullYear()}</p>
+  </footer>
+ );
+}
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+ return(
+  <header>
+   <h1>Vite Project 101</h1>
+   <nav>
+    <ul><a href="#">Home</a></ul>
+    <ul><a href="#">About</a></ul>
+    <ul><a href="#">Services</a></ul>
+   </nav>
+   <hr/>
+  </header>
+ );
+}
+export default Header;
