0b9ae95416c040d238160f2e3d9b7bc98bf0dbf2
Pasan <pasankg@gmail.com>
2024-07-28 12:30:37 +1000

    Inline styles vs module styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 // import Header from "./Header.jsx"
 // import Footer from "./Footer.jsx"
 // import Food from "./Food.jsx"
-import Card from "./Card"
+// import Card from "./Card"
+
+import ButtonModule from "./Button/Button"
+import ButtonInline from "./Button"
+
 function App() {
   return(
     <>
-      <Card/>
+      <ButtonModule/>
+      <ButtonInline/>
     </>
   )
 }
---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+
+function Button() {
+  const styles = {
+    backgroundColor: "rgb(105, 227, 254)",
+    color: "#ffff",
+    border: "none",
+    borderRadius: "5px",
+    padding: "5px",
+  }
+  return <button style={styles}>Click Me</button>;
+}
+export default Button;
