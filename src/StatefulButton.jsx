31ab8656a3db2f4396235128b9502077e4402b54
Pasan <pasankg@gmail.com>
2024-07-28 20:16:03 +1000

    using useState for stateful variables

--- src/StatefulButton.jsx
+++ src/StatefulButton.jsx
@@ -6,16 +6,36 @@ function StatefulButton() {
   // a varaible and a setter function.
   // we are using array destructuring.
   const [name, setName] = useState("Guest");
+  const [age, setAge] = useState(0);
+  const [isEmployed, setEmployed] = useState(0);
 
   const changeName = () => {
     setName("Spongebob");
   };
+
+  const incrementAge = () => {
+   setAge(age+1);
+  }
+
+  const toggleEmployed = () => {
+   setEmployed(!isEmployed);
+  }
 
   return (
     <>
       <p>Name is &nbsp;{name}</p>
       <button className={styles.button} onClick={changeName}>
         Set Name
+      </button>
+
+      <p>Age is &nbsp;{age}</p>
+      <button className={styles.button} onClick={incrementAge}>
+        Increment Age
+      </button>
+      
+      <p>Is Employed &nbsp;{isEmployed ? "Yes" : "No"}</p>
+      <button className={styles.button} onClick={toggleEmployed}>
+        Toggle Status
       </button>
     </>
   );
