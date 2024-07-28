cc96b0719481261b332fac4c079b10bd97293eda
Pasan <pasankg@gmail.com>
2024-07-28 19:32:13 +1000

    Button click events

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@
 // import Footer from "./Footer.jsx"
 // import Food from "./Food.jsx"
 // import Card from "./Card"
-// import ButtonModule from "./Button/Button"
+import ButtonModule from "./Button/Button"
 // import ButtonInline from "./Button"
 // import Student from "./Student";
 // import UserGreeting from "./UserGreeting";
 // import List from "./List";
-import ListModule from "./List.module";
+// import ListModule from "./List.module";
 
 function App() {
   // return (
@@ -34,29 +34,35 @@ function App() {
   //   </>
   // );
 
-  const fruits = [
-    { id: 1, name: "apple", calories: 95 },
-    { id: 2, name: "orange", calories: 45 },
-    { id: 3, name: "mango", calories: 105 },
-    { id: 4, name: "banana", calories: 159 },
-    { id: 5, name: "pineapple", calories: 37 },
-  ]; 
-  const vegetables = [
-    { id: 1, name: "brocolli", calories: 95 },
-    { id: 2, name: "carrot", calories: 45 },
-    { id: 3, name: "pumpkin", calories: 105 },
-    { id: 4, name: "tomato", calories: 159 },
-    { id: 5, name: "beatroot", calories: 37 },
-  ]; 
+  // const fruits = [
+  //   { id: 1, name: "apple", calories: 95 },
+  //   { id: 2, name: "orange", calories: 45 },
+  //   { id: 3, name: "mango", calories: 105 },
+  //   { id: 4, name: "banana", calories: 159 },
+  //   { id: 5, name: "pineapple", calories: 37 },
+  // ]; 
+  // const vegetables = [
+  //   { id: 1, name: "brocolli", calories: 95 },
+  //   { id: 2, name: "carrot", calories: 45 },
+  //   { id: 3, name: "pumpkin", calories: 105 },
+  //   { id: 4, name: "tomato", calories: 159 },
+  //   { id: 5, name: "beatroot", calories: 37 },
+  // ]; 
+
+  // return(
+  //   <>    
+  //     {fruits.length > 0 ? <ListModule items={fruits} category="Fruits" /> : null}
+      
+  //     {/* short circuiting above */}
+  //     { vegetables.length > 0 && <ListModule items={vegetables} category="Vegitables" /> }
+      
+  //     <ListModule />
+  //   </>
+  // );
 
   return(
-    <>    
-      {fruits.length > 0 ? <ListModule items={fruits} category="Fruits" /> : null}
-      
-      {/* short circuiting above */}
-      { vegetables.length > 0 && <ListModule items={vegetables} category="Vegitables" /> }
-      
-      <ListModule />
+    <>
+    <ButtonModule/>
     </>
   );
 }
--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,6 +1,23 @@
 import styles from "./Button.module.css";
 
 function Button() {
-  return <button className={styles.button}>Click Me</button>;
+  const handleClick = (name) => console.log(`${name} stop clicking me !`)
+
+  const handleClick2 = (name) => console.log(`${name} stop clicking me !`)
+
+  const handleClick3 = (e) => console.log(e.target.textContent = "OUCH");
+  
+  return (
+    <>
+      <button onClick={handleClick("Yo")} className={styles.button}>Click Me !</button>
+      
+      {/* If we leave the function as above just inside of {}, JS will invoke the function at application start. */}
+      {/* To avoid this behaviour, we use function expression or an arrow function. */}
+      <button onClick={() => handleClick2("Bro")} className={styles.button2}>Click Me !!</button>
+
+      <button onDoubleClick={(e) => handleClick3(e)} className={styles.button3}>Double click me !!!</button>
+
+    </>
+  );
 }
 export default Button;
