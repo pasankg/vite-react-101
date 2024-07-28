e0d571e3c823f634411b1e49b9b6d5b10caf48d6
Pasan <pasankg@gmail.com>
2024-07-28 12:47:10 +1000

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@
 // import Footer from "./Footer.jsx"
 // import Food from "./Food.jsx"
 // import Card from "./Card"
+// import ButtonModule from "./Button/Button"
+// import ButtonInline from "./Button"
 
-import ButtonModule from "./Button/Button"
-import ButtonInline from "./Button"
+import Student from "./Student";
 
 function App() {
-  return(
+  return (
     <>
-      <ButtonModule/>
-      <ButtonInline/>
+      {/* If the value is not a string, wrap it with {} */}
+      <Student name="Spongebob" age={20} isStudent={true} />
+      <Student name="Patrick" age={21} isStudent={true} />
+      <Student name="Squidward" age={30} isStudent={false} />
+      <Student name="Sandy" age={26} isStudent={false} />
+      <Student name="Larry" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
